5b14c08c4b7b89be4ee4ff798ae5fd33456511a3
nravitej <61945844+nravitej@users.noreply.github.com>
2022-06-22 10:27:04 +0200

    heroku details

--- FastAPI/FastMain.py
+++ FastAPI/FastMain.py
@@ -14,11 +14,11 @@ import urllib
 import sqlalchemy
 
 app = FastAPI()
-host_server = os.environ.get('host_server', 'localhost')
-db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
-database_name = os.environ.get('database_name', 'postgres')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'super')))
+host_server = os.environ.get('Host', 'ec2-3-230-122-20.compute-1.amazonaws.com')
+db_server_port = urllib.parse.quote_plus(str(os.environ.get('Port', '5432')))
+database_name = os.environ.get('Database', 'd59neqiqc668vc')
+db_username = urllib.parse.quote_plus(str(os.environ.get('User', 'otbgdaygwxniqq')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('Password', '741b2f1e4cb951ab402009d479b9073f765ee5234f0f36537ee592f9ebaec8e0')))
 ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode', 'prefer')))
 DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port,
                                                                database_name, ssl_mode)
